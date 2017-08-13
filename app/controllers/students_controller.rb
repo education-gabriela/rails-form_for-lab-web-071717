a05942bd293b9be9d6bcc3126068c0dcd02ec5ff
gabidavila <gabidavila@gmail.com>
2017-08-13 09:20:32 -0400

    Created crud for Students

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,39 @@
+class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update]
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(post_params)
+    redirect_to student_path(@student)
+  end
+
+  def edit
+
+  end
+
+  def update
+    @student.update(post_params)
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def post_params
+    params.require(:student)
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+end
